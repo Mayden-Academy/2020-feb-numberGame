eaf5c5887c32156d36dd7f8eb9d1de90251e8795
christianturner@hotmail.co.uk <academy@lpt00361mac.home>
2020-04-06 18:18:08 +0100

    removed cycle parameter and uses player score instead

--- main.js
+++ main.js
@@ -54,28 +54,29 @@ function generateHardNumbers(numbersWanted){
     return allNumbers;
 }
 
-function generateNumbers(cycle = 0) {
+function generateNumbers(Score = 0) {
     let numberArray = [];
     let numbersWanted = Math.floor(Math.random() * 3) + 3;
 
-    if (cycle < 2) {
+    if (Score < 2) {
         numberArray = generateEasyNumbers(numbersWanted);
-    } else if ((cycle === 3) || (cycle === 4)) {
+    } else if ((Score === 3) || (Score === 4)) {
         numberArray = generateNegativeNumbers(numbersWanted);
-    } else if ((cycle === 5) || (cycle === 6)) {
+    } else if ((Score === 5) || (Score === 6)) {
         numberArray = generateNegativeNumbers(numbersWanted - 2).concat(generateHardNumbers(2));
-    } else if ((cycle === 7) || (cycle === 8)) {
+    } else if ((Score === 7) || (Score === 8)) {
         numberArray = generateNegativeNumbers(numbersWanted - 3).concat(generateHardNumbers(3));
-    } else if ((cycle === 9) || (cycle === 10)) {
+    } else if ((Score === 9) || (Score === 10)) {
         numberArray = generateHardNumbers(numbersWanted - 2).concat(generateHardNumbers(2));
     } else {
         numberArray = generateHardNumbers(numbersWanted);
     }
+    
     return shuffleArray(numberArray);
 }
 
 
-function displayNumbers(randomNumbers, currentScore, cycle) {
+function displayNumbers(randomNumbers, currentScore) {
     const sortedNumbers = randomNumbers.slice(0).sort((a, b) => a - b);
     const numbersOnScreen = randomNumbers.length;
     let selectedOrder = [];
@@ -109,28 +110,26 @@ function displayNumbers(randomNumbers, currentScore, cycle) {
             }
 
             if (JSON.stringify(selectedOrder) === JSON.stringify(sortedNumbers)) {
-                cycle++;
                 currentScore++;
-                const randNumbers = generateNumbers(cycle);
+                const randNumbers = generateNumbers(currentScore);
 
                 document.querySelector('#player_score').textContent = `Score: ${currentScore}`;
                 document.querySelector('#play_area').textContent = '';
-                displayNumbers(randNumbers, currentScore, cycle);
+                displayNumbers(randNumbers, currentScore);
             }
         });
     });
 }
 
 document.querySelector('#start').addEventListener('click', () => {
-    const firstCycle = 0;
     const startingScore = 0;
-    const randNumbers = generateNumbers(firstCycle);
+    const randNumbers = generateNumbers(startingScore);
 
     document.querySelector('#splash').style.display = 'none';
     document.querySelector('#game').style.display = 'block';
 
     displayTimeLeft(totalTime);
     document.querySelector('#player_score').textContent = `Score: ${startingScore}`;
-    displayNumbers(randNumbers, startingScore, firstCycle);
+    displayNumbers(randNumbers, startingScore);
     timer();
 });
