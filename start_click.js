83b4cd0e198fdadd9e1bfc27020554053c4740bc
Amanda Hardman <amandahrdmn@yahoo.com>
2020-04-06 10:00:56 +0100

    merged with task3b (numbers functions)

--- start_click.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('#start').addEventListener('click', () => {
-    document.querySelector('#splash').style.display = 'none';
-    document.querySelector('#game').style.display = 'block';
-})
