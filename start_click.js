e6b3a79ff4e15d6722a3221f4f25b223c3f89294
Amanda Hardman <amandahrdmn@yahoo.com>
2020-04-06 09:51:33 +0100

    link start button game screen

---
+++ start_click.js
@@ -0,0 +1,4 @@
+document.querySelector('#start').addEventListener('click', () => {
+    document.querySelector('#splash').style.display = 'none';
+    document.querySelector('#game').style.display = 'block';
+})
